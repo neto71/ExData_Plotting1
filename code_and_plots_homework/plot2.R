425490beb3fc0aada33c20f4fd1728f3aa44d54d
Ernesto N.Santillan Guerrero <ernesto.santillan@gmail.com>
2015-01-09 10:52:04 -0800

    update to plot t

--- code_and_plots_homework/plot2.R
+++ code_and_plots_homework/plot2.R
@@ -8,11 +8,11 @@ powerData<-subset(powerData,powerData$Date >="2007-02-01" & powerData$Date <="20
 powerData$Date1<-paste(powerData$Date,powerData$Time)
 powerData$Date1<-as.POSIXlt(powerData$Date1,format="%Y-%m-%d %H:%M:%S")
 
-par(cex.lab=.85,cex.axis=.75)
+par(cex.axis=1.1)
 with(powerData,{
     plot(Date1,Global_active_power,type="n",xlab="",ylab="")
     lines(Date1,Global_active_power,type="l")
-    title(ylab="Global Active Power (kilowatts)")
+    title(ylab="Global Active Power (kilowatts)",cex.lab=1.2)
     })
 dev.copy(png,file="plot2.png",width=480,height=480)
 dev.off()
